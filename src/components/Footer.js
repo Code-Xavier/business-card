ea687bc7e08cc370874ff1fff67957b2a04c3d9b
Xavier Valois-Smith <96591678+Code-Xavier@users.noreply.github.com>
2022-07-15 11:30:53 -0700

    "fixed footer links"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import github from "../images/github.svg";
 function Footer() {
   return (
     <div className="footer">
-      <a href="https://twitter.com/themusicalcoder"><img src={twitter} alt="twitter" /></a>
-      <a href="https://www.instagram.com/themusicalcoder/"><img src={instagram} alt="instagram" /></a>
-      <a href="https://github.com/Code-Xavier"><img src={github} alt="github" /></a>
+      <a href="https://twitter.com/themusicalcoder" target="_blank"><img src={twitter} alt="twitter" /></a>
+      <a href="https://www.instagram.com/themusicalcoder/" target="_blank"><img src={instagram} alt="instagram" /></a>
+      <a href="https://github.com/Code-Xavier" target="_blank"><img src={github} alt="github" /></a>
     </div>
   );
 }
